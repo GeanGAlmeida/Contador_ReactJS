5619ff3b06379735121d44629c285e42606443e6
Gean Almeida <gomesgean81@gmail.com>
2024-05-18 13:20:07 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function Contador() {
+  const[contador, setContador] = useState(0);
+
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
+
+  const decrementar = () =>{
+    setContador(contador - 1);
+  }
+
+  return(
+    <div>
+      <h2>Contador</h2>
+      <p>Valor: {contador}</p>
+      <button onClick={decrementar}>Decrementar</button>
+      <button onClick={incrementar}>Incrementar</button>
+    </div>
+  )
+}
+
+export default Contador
